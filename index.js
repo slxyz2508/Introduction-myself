c593a308962123875d0af69c0964497cceff9746
SLX <126223816+slxyz2508@users.noreply.github.com>
2023-10-12 03:05:17 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+
+</head>
+<body class="bg-dark text-white position-relative">
+
+   
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">My Profile</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/slxyz2508">Github</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.facebook.com/profile.php?id=100078099725240&mibextid=9R9pXO">Facebook</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.instagram.com/slaughterxyz/">Instagram</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<video autoplay muted loop class="position-absolute top-0 left-0 min-w-100 min-h-100">
+    <source src="y2mate.com - Galaxy 2023    Live Wallpaper 4k ULTRA HD 60fps  _1080p.mp4" type="video/mp4"> 
+</video>
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <img src="371494297_2982476425219065_5525652394754928687_n.png" alt="Responsive image" class="img-fluid mx-auto w-25">
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-12 text-center">
+            <h2 class="text-info">Who am I?</h2>
+            <p class="lead text-light">Thanawat Meechamnan Ton</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h3 class="text-success">Quotes</h3>
+            <ul class="list-unstyled">
+                <li class="text-light">A person without dreams is as good as dead.</li>
+                <li class="text-light">คนที่ไม่มีความฝันเท่ากับตาย</li>
+                
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div id="carouselExample" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="col-md-12 text-center">
+            <h3 class="text-warning">Portfolio</h3>
+        <div class="carousel-item active">
+            <img src="01-1.png" class="d-block w-50 mx-auto" alt="Image 1">
+        </div>
+        <div class="carousel-item">
+            <img src="01-2.png" class="d-block w-50 mx-auto" alt="Image 2">
+        </div>
+  
+    </div>
+    <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+        <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-danger text-center">Goals</h3>
+            <address class="text-light text-center">
+                University : Chulalongkorn University<br>
+                Faculty : Faculty of Science<br>
+                Branch : Computer Science<br>
+                
+            </address>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-primary text-center">More about me</h3>
+            <address class="text-light text-center">
+                Hobby : Play Games, Listen to music,Read a cartoon <br>
+                Color : Black,Red <br>
+                Food : Noodles
+            </address>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+</body>
